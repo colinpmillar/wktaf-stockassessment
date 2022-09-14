77a2d5d4c5aff8901690c8ec2f8b76ddb82d9f6b
colinpmillar <colin.millar@ices.dk>
2022-09-14 12:41:18 +0200

    data script done

---
+++ data.R
@@ -0,0 +1,52 @@
+## Preprocess data, write TAF data tables
+
+## Before:
+## After:
+
+## load libraries
+library(icesTAF)
+library(stockassessment)
+
+# ensure directory
+mkdir("data")
+
+## 1  Read underlying data from bootstrap/data
+
+#  ## Catch-numbers-at-age ##
+catage <- read.ices("bootstrap/data/sam_data/cn.dat")
+
+#  ## Catch-weight-at-age ##
+wcatch <- read.ices(taf.data.path("sam_data/cw.dat"))
+wdiscards <- read.ices(taf.data.path("sam_data/dw.dat"))
+wlandings <- read.ices(taf.data.path("sam_data/lw.dat"))
+
+#  ## Natural-mortality ##
+natmort <- read.ices(taf.data.path("sam_data/nm.dat"))
+
+#  ## Proportion of F before spawning ##
+propf <- read.ices(taf.data.path("sam_data/pf.dat"))
+
+#  ## Proportion of M before spawning ##
+propm <- read.ices(taf.data.path("sam_data/pm.dat"))
+
+#  ## Stock-weight-at-age ##
+wstock <- read.ices(taf.data.path("sam_data/sw.dat"))
+
+# Landing fraction in catch at age
+landfrac <- read.ices(taf.data.path("sam_data/lf.dat"))
+
+## 2 Preprocess data
+
+# landings
+latage <- catage * landfrac[, -1]
+datage <- catage * (1 - landfrac[, -1])
+
+## 3 Write TAF tables to data directory
+write.taf(
+  c(
+    "catage", "latage", "datage", "wstock", "wcatch",
+    "wdiscards", "wlandings", "natmort", "propf", "propm",
+    "landfrac"
+    ),
+  dir = "data"
+)
